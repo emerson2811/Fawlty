e89ba5cc477bb400e7262276fe866c0a15c52217
Adam <adamcemerson@icloud.com>
2017-11-15 09:22:29 -0600

    Added API call

--- Call.py
+++ Call.py
@@ -24,7 +24,7 @@ parameters = {"lat": 37.78, "lon": -122.41}
 response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
 
 # Get the response data as a python object.  Verify that it's a dictionary.
-data = response.json()
+#data = response.json()
 print(type(data))
 print(data)
 
