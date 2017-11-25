48f2442427a827eab84f2fd8297e33511437933f
emerson2811 <adam.emerson@sanfordhealth.org>
2017-11-24 18:49:55 -0600

    Added errors and exceptions handling, functions, BMI, and file reading

--- filehandling.py
+++ filehandling.py
@@ -1,4 +1,23 @@
-file = open("demo.txt", 'w')
-#do something with the file
+# file = open("demo.txt", 'w')
+# #do something with the file
+# file.close()
+# #closes the file - always close files
+#
+# file = open ("nonsense.txt", 'r')
+# content = file.readline()
+# #opens a file, reads the content of the file into the "content" varialble
+# print(content)
+# file.close()
+
+file = open("nonsense.txt", 'w')
+file.write("text written to file")
 file.close()
-#closes the file - always close files
+
+file = open("nonsense.txt", 'r')
+content = file.read()
+print(content)
+file.close()
+
+file = open("nonsense.txt", 'a')
+file.write("this is the new line")
+file.close()
