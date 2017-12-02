891a98d4d77afb69fecf59fb65a57a4257d7672d
emerson2811 <adam.emerson@sanfordhealth.org>
2017-12-02 15:35:41 -0600

    Added errors and exceptions handling, functions, BMI, and file reading

---
+++ Sets.py
@@ -0,0 +1,16 @@
+numbers = {1, 2, 3, 4, 5, 6}
+print(numbers)
+
+numbers.add(9)
+print(numbers)
+
+seta = {1, 2, 3, 4, 5}
+setb = {4, 5, 6, 7, 8}
+
+print(seta | setb)
+#union of set a and set b
+
+print(seta & setb)
+#finds the common number between set a and set b
+
+print(seta - setb)
