89a266c88d2d4006c6062318b09f4a8395617c7f
emerson2811 <adam.emerson@sanfordhealth.org>
2017-12-02 22:01:55 -0600

    Added errors and exceptions handling, functions, BMI, and file reading

--- itertools.py
+++ itertools.py
@@ -8,5 +8,8 @@ for i in count(3):
     if i >= 20:
         break
 
-from itertools import accumulate
+from itertools import accumulate, takewhile
 
+numbers = list(accumulate(range(8)))
+print(numbers)
+print(list(takewhile(lambda x: x <= 10.numbers)))
