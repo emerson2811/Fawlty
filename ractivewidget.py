70c6be110a9f45872ff6025f69b9fc7962e1ef54
emerson2811 <adam.emerson@sanfordhealth.org>
2017-12-11 20:57:16 -0600

    data encapsulation lesson and operator overloading lesson

--- ractivewidget.py
+++ ractivewidget.py
@@ -1,9 +1,9 @@
-from tkinter import *
+import tkinter
 
-root = Tk()
+root = tkinter.Tk()
 
 label1 = label(root, text="First", bg="Red", fg="White")
 label1.pack(fill=X)
 
 label2 = Label(root, text="Second", bg="Blue", fg="Green")
-label2.pack(side=LEFT.fill=Y)
+label2.pack(side=LEFT, fill=Y)
