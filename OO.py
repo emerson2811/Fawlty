224e84eee9edfd5cbc7f733a51353bc33f80d239
emerson2811 <adam.emerson@sanfordhealth.org>
2017-12-13 21:23:22 -0600

    data encapsulation lesson and operator overloading lesson

--- OO.py
+++ OO.py
@@ -1,20 +1,46 @@
 from tkinter import *
 
-class MyButtons:
-    def _init_(self, rootone):
-        frame = Frame(rootone)
-        frame.pack()
+root = TK()
 
-        self.printbutton = Button(frame, text="Click Here", command=self.printmessge)
-        self.printbutton.pack()
+mymenu = Menu(root)
+root.config(menu=mymenu)
 
-        self.quitbutton = Button(frame, text="Exit", command=frame.quit)
-        self.quitbutton.pack(side=LEFT)
+submenu = Menu(mymenu)
 
-    def printmessage(self):
-        print("Button Clicked")
+mymenu.add_cascade(label="File", menu=submenu)
 
-root = Tk()
-b = MyButtons(root)
+submenu.add_command(label="project", command=function1)
+submenu.add_command(label="Save", command=function1)
+
+submenu.add_seperator()
+submenu.add_command(label="Exit", command=function1)
+
+toolbar = Frame(root, bg="pink")
+insertbutton = Button(toolbar, text = "Insert Files", command=function1)
+insertbutton.pack(side=LEFT, padx=2, pady=3)
+
+printbutton = Button(toolbar, text="print", command=function1)
+printbutton.pack(side=LEFT, padx=2, pady=3)
+
+toolbar.pack(side=TOP, fill=x)
 
 root.mainloop()
+
+# class MyButtons:
+#     def _init_(self, rootone):
+#         frame = Frame(rootone)
+#         frame.pack()
+#
+#         self.printbutton = Button(frame, text="Click Here", command=self.printmessge)
+#         self.printbutton.pack()
+#
+#         self.quitbutton = Button(frame, text="Exit", command=frame.quit)
+#         self.quitbutton.pack(side=LEFT)
+#
+#     def printmessage(self):
+#         print("Button Clicked")
+
+# root = Tk()
+# b = MyButtons(root)
+#
+# root.mainloop()
